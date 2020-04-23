72de82683b22383a78535a45ad1f59d928396d14
chedadsp <nenad.cetic@gmail.com>
2020-04-23 23:45:12 +0200

    Adding button with debounce

--- espkeys32.ino
+++ espkeys32.ino
@@ -5,25 +5,54 @@
 
 BleKeyboard bleKeyboard;
 
+const int buttonPin = 15;     // the number of the pushbutton pin
+
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Hello espkeys32!!!");
   bleKeyboard.begin();
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  if(bleKeyboard.isConnected()) {
-    Serial.println("Sending espkeys32...");
-    bleKeyboard.print("MyPassWord123");
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
 
-    delay(1000);
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
 
-    Serial.println("Sending Enter key...");
-    bleKeyboard.write(KEY_RETURN);
-
-    delay(1000);
-
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
   }
-  Serial.println("Waiting 5 seconds...");
-  delay(5000);
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        Serial.println("Sending espkeys32...");
+        bleKeyboard.print("MyPassWord123");
+        bleKeyboard.write(KEY_RETURN);
+      }
+    }
+  }
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
 }
